1102dfebc208925f58bda72ff70ab3e76f18f292
TiagoMendes <tiagovazmendes7@gmail.com>
2019-11-09 00:40:57 +0000

    Já faz troca de mensagem usando ssl

---
+++ Client.py
@@ -0,0 +1,31 @@
+import socket, ssl
+
+sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+ssl_sock = ssl.wrap_socket(sock, ca_certs="/Users/tiagomendes/Desktop/SIRS-Project/SIRS26-2019_V1/cert.pem", cert_reqs = ssl.CERT_REQUIRED)
+
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+
+try:
+	ssl_sock.connect((HOST, PORT))
+	print( ">> Conexão estabelecida!" )
+except:
+	print( ">> Conexão Perdida!" )
+
+
+#f = open( "C:\\Users\\Documents\\Projecto\\demo.jpg", "rb" )
+
+data = b"OLA"
+
+#data = f.readlines()
+#for line in data:
+ssl_sock.send(data)
+
+EOF = b"\n\r##"
+ssl_sock.send(EOF)
+#f.close()
+
+ssl_sock.close()
+print( ">> Envio terminado!" )
---
+++ Server.py
@@ -0,0 +1,63 @@
+import socket, ssl
+
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+
+class ServerSocket:
+	
+	def socketConnect(self):
+		
+		print (">>Socket Criado a Aguardar Conexao")
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.bind((HOST, PORT))
+		self.sock.listen(1)
+	
+		# Receber Conexao
+		try:
+			self.newsocket, self.clientSocket = self.sock.accept()
+			print (">>Existe uma tentatica de Conexao")
+				
+			self.connssl = ssl.wrap_socket(self.newsocket, server_side=True, certfile = "/Users/tiagomendes/Desktop/SIRS-Project/SIRS26-2019_V1/cert.pem", keyfile = "/Users/tiagomendes/Desktop/SIRS-Project/SIRS26-2019_V1/certkey.pem", ssl_version=ssl.PROTOCOL_TLSv1)
+
+		except Exception as err:
+			print (">>!!Este Utilizador nao tem permissao!!\n")
+			print(err)
+	
+		print (">>Conexao Recebida")
+		print ( ">>>IP Remoto: %s" % self.clientSocket[0] )
+		print ( ">>>Porta Remota: %d" % self.clientSocket[1] )
+		
+		
+		# Receber mensagem
+		try:
+			print (">>A Decorrer Transferencia")
+			self.data = b""
+			while (self.data[-4:] != b"\n\r##"):
+				self.data += self.connssl.recv(1024)
+			
+		
+			self.data = self.data.replace(b"\n\r##", b"")
+			print (">>>Mensagem Recebida : %s" % self.data)
+			
+			#self.imageOut = open( "C:\\Users\\Documents\\Projecto\\demo_out.jpg", "wb" )
+			#self.imageOut.write(self.data)
+			#self.imageOut.close()
+			print (">>Transferencia Concluida")
+			
+		except Exception as err:
+			print (">>!!Falhou a Transferencia!!\n")
+			print(err)
+		
+		
+		self.newsocket.close()
+		self.sock.close()
+		print (">>Socket finalizado")
+
+
+
+x = ServerSocket()
+x.socketConnect()
+
+
