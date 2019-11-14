6fcbdd15a776eb95acdcb1cf92742e8ed028ea49
root <kevin-batistac@hotmail.com>
2019-11-14 16:22:09 +0000

    More queries added

--- DB_Scoreboard.py
+++ DB_Scoreboard.py
@@ -35,3 +35,52 @@ def get_group_scoreboard(group_id):
 			cursor.close()
 			db.close()
 
+def get_user_score(user_id):
+	try:
+		db = connect()
+
+		user_id = str(user_id)
+		cursor = db.cursor(prepared=True)
+		query = "SELECT username,points,num_vul FROM Scoreboard WHERE user_id=%s LIMIT 1"
+		parameters = (user_id)
+		cursor.execute(query,parameters)
+		result = cursor.fetchone()
+
+		print(result)
+		return result;
+
+
+	except Exception as e:
+		# UNCOMENT FOR DATABASE LOGIN DEBUG
+		# print("Error connecting to the server")
+		print(e)
+
+	finally:
+		if db.is_connected():
+			cursor.close()
+			db.close()
+
+def vulnerability_exist(fingerprint):
+	try:
+		db = connect()
+
+		fingerprint = str(fingerprint)
+		cursor = db.cursor(prepared=True)
+		query = "SELECT fingerprint FROM Vulnerability WHERE fingerprint=%s LIMIT 1"
+		parameters = (fingerprint)
+		cursor.execute(query,parameters)
+		result = cursor.fetchone()
+
+		print(result)
+		return result;
+
+
+	except Exception as e:
+		# UNCOMENT FOR DATABASE LOGIN DEBUG
+		# print("Error connecting to the server")
+		print(e)
+
+	finally:
+		if db.is_connected():
+			cursor.close()
+			db.close()
--- DB_User.py
+++ DB_User.py
@@ -45,3 +45,39 @@ def authenticate(user, password):
 		if db.is_connected():
 			cursor.close()
 			db.close()
+
+def authorizated(username, auth_type):
+	try:
+		db = connect()
+
+		# UNCOMENT FOR USER & PASS CONSOLE INPUT
+		# username = raw_input("Insert your username:")
+		# password = raw_input("Insert your password:")
+
+		# Username and Password coming from Server.py
+		username = str(username)
+		auth_type = str(auth_type)
+		cursor = db.cursor(prepared=True)
+		query = "SELECT username FROM Users WHERE username=%s AND auth_type=%s;"
+		parameters = (username,auth_type)
+		cursor.execute(query,parameters)
+		result = cursor.fetchone()
+
+		if result != None:
+			# UNCOMENT FOR DATABASE LOGIN DEBUG
+			# print('Login Successful')
+			return True;
+		else:
+			return False;
+			# sys.exit()
+
+
+	except Exception as e:
+		# UNCOMENT FOR DATABASE LOGIN DEBUG
+		# print("Error connecting to the server")
+		print(e)
+
+	finally:
+		if db.is_connected():
+			cursor.close()
+			db.close()
