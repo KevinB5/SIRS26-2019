f2eaeba34ae7be93c5e3c424ebc8cc6681359a2c
root <kevin-batistac@hotmail.com>
2019-11-16 15:47:35 -0500

    Database add vulnerabilities, with fingerprint and increment points on users.

--- DB_Scoreboard.py
+++ DB_Scoreboard.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import sys
+import hashlib
 
 def connect():
 	db = mysql.connector.connect(
@@ -60,14 +61,70 @@ def get_user_score(user_id):
 			cursor.close()
 			db.close()
 
+def add_score_vulnerability(user_id,points,name_vul):
+	# When private key system is done, use:  user_id+private_key+name_vul
+	content = str(user_id)+name_vul
+	fingerprint = hashlib.sha256(content.encode()).hexdigest()
+
+	if(vulnerability_exist(fingerprint)):
+		return False
+
+	else:
+		try:
+			db = connect()
+
+			user_id = str(user_id)
+			name_vul = str(name_vul)
+
+			cursor = db.cursor(prepared=True)
+			query = "SELECT points,num_vul FROM Scoreboard WHERE user_id=%s"
+			parameters = (user_id)
+			cursor.execute(query,parameters)
+			result = cursor.fetchone()
+			print(result)
+
+			updated_points=result[0]+points
+			updated_num_vul=result[1]+1
+			print(user_id,' update ',updated_points,' ',updated_num_vul)
+			
+			cursor = db.cursor(prepared=True)
+			query = "UPDATE Scoreboard SET points=%s,num_vul=%s WHERE user_id=%s"
+			parameters = [updated_points,updated_num_vul,user_id]
+			cursor.execute(query,parameters)
+			db.commit()
+			#print('updated')
+			#print(cursor.rowcount,' records affected')
+			
+			cursor = db.cursor(prepared=True)
+			query = "INSERT INTO Vulnerability (user_id,fingerprint,name_vul) VALUES (%s,%s,%s)"
+			parameters = [user_id,fingerprint,name_vul]
+			cursor.execute(query,parameters)
+			db.commit()
+			print('inserted')
+			#print(cursor.rowcount,' records affected')
+			
+			return True
+
+		except Exception as e:
+			# UNCOMENT FOR DATABASE LOGIN DEBUG
+			# print("Error connecting to the server")
+			print(e)
+
+		finally:
+			if db.is_connected():
+				cursor.close()
+				db.close()
+
 def vulnerability_exist(fingerprint):
 	try:
 		db = connect()
 
 		fingerprint = str(fingerprint)
+
+		print(fingerprint)
 		cursor = db.cursor(prepared=True)
-		query = "SELECT fingerprint FROM Vulnerability WHERE fingerprint=%s LIMIT 1"
-		parameters = (fingerprint)
+		query = "SELECT id_vul FROM Vulnerability WHERE fingerprint=%s"
+		parameters = [fingerprint]
 		cursor.execute(query,parameters)
 		result = cursor.fetchone()
 
@@ -86,5 +143,4 @@ def vulnerability_exist(fingerprint):
 			db.close()
 
 
-#TODO: update score from user
-#TODO: add vulnerability
+print(add_score_vulnerability(2,100,'vuln2'))
