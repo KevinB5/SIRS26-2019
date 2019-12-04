c7221821f98d5657afd3ec65b33177e261ca6eb2
Tiago Santos <tiagokinasmiranda@gmail.com>
2019-12-04 18:04:48 +0000

    hash enconding

--- Server/DB_User.py
+++ Server/DB_User.py
@@ -1,4 +1,5 @@
 import mysql.connector, sys, HashPwd
+import System_log, Server_NS
 
 def connect():
 	db = mysql.connector.connect(
@@ -31,13 +32,15 @@ def authenticate(user, password):
 
 		# to check if the hash matches the password
 		hash = HashPwd.Hash(bytes(password, "utf-8"), user)
-
 		System_log.writeSystemLog('Database Users','Connection successful','info')
-		if( hash.check_password(bytes(result[1], "utf-8")) == True ):
+		# if( hash.check_password(bytes(result[1],"utf-8")) == True ):
+		if( hash.check_password(bytes(result[1])) == True ):
 			# UNCOMENT FOR DATABASE LOGIN DEBUG
 			# print('Login Successful')
+			print("#1")
 			return True;
 		else:
+			print("#2")
 			return False;
 			# sys.exit()
 
